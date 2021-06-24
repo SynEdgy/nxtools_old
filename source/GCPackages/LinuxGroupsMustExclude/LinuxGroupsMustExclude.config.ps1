c2aa78641b1e29dd2f2f29ed748afff6601aa8ec
gaelcolas <gaelcolas@synedgy.com>
2021-06-24 15:40:24 +0200

    GC packages without test (waiting for kitchen-pester release)

--- source/GCPackages/LinuxGroupsMustExclude/LinuxGroupsMustExclude.config.ps1
+++ source/GCPackages/LinuxGroupsMustExclude/LinuxGroupsMustExclude.config.ps1
@@ -1,10 +1,12 @@
 configuration LinuxGroupsMustExclude {
-    Import-DscResource -ModuleName nxtools -ModuleVersion 0.2.0
+    Import-DscResource -ModuleName nxtools -ModuleVersion 0.3.0
 
+    node LinuxGroupsMustExclude {
         nxGroup LinuxGroupsMustExclude {
             Ensure =  'Present'
             GroupName =  'foobar'
             PreferredGroupID = 1005
             MembersToExclude = 'root','gcolas'
         }
+    }
 }
