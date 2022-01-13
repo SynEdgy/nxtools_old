3eff74768c8b6e30dff90dffcb07a641b60fb809
gaelcolas <gaelcolas@synedgy.com>
2022-01-13 16:26:59 +0100

    skip unfinished test

--- tests/Unit/Public/Set-nxFileLine.tests.ps1
+++ tests/Unit/Public/Set-nxFileLine.tests.ps1
@@ -1,5 +1,5 @@
-Describe 'Set-nxFileLine function' {
+Describe 'Set-nxFileLine function' -Skip {
     it 'should not throw' {
-        gc $PSScriptRoot/assets/test.txt | % { $_ -replace '^pretty\s(?<adj>easy)','very ${adj}'} | Set-Content -Path $PSScriptRoot/assets/test.txt
+        #gc $PSScriptRoot/assets/test.txt | % { $_ -replace '^pretty\s(?<adj>easy)','very ${adj}'} | Set-Content -Path $PSScriptRoot/assets/test.txt
     }
 }
